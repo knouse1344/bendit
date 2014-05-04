833e50fcb2c1362ec87792c90f839af2c3a73144
Bryan <bknouse@u.rochester.edu>
2014-05-04 14:00:33 -0400

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Bendit::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'main#index'
 
+  get 'main/about'
+  get 'main/index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
