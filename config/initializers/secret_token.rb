fd656914afa2b0fd7a2350a85e31078ca4280c6d
Bryan <bknouse@u.rochester.edu>
2014-05-04 12:25:56 -0400

    asldfjk

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bendit::Application.config.secret_key_base = 'f510383f75877c3ca779ee882b23538309564e0590197892c7228433cd07ea322ee34deda2d439894d94b5497490f805cbccc5f2cc3f0616564ff6bd47c7666e'
